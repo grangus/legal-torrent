e71f30c5e2b04183b7b4ba975b3ae142d07c9e22
grangus <31970023+grangus@users.noreply.github.com>
2021-03-10 18:02:57 -0600

    added some more session methods

--- server/src/misc/redis/index.ts
+++ server/src/misc/redis/index.ts
@@ -6,6 +6,18 @@ export class Redis {
   }
 
   async addUserSession(userId: number, sessionId: string) {
-    return await this.client.sadd(`user:${userId}:session`, sessionId);
+    return await this.client.sadd(`user:${userId}:sessions`, sessionId);
+  }
+
+  async removeUserSession(userId: number, sessionId: string) {
+    return await this.client.srem(`user:${userId}:sessions`, sessionId);
+  }
+
+  async getAllUserSessions(userId: number) {
+    return await this.client.smembers(`user:${userId}:sessions`);
+  }
+
+  async removeAllUserSessions(userId: number) {
+    return await this.client.del(`user:${userId}:sessions`);
   }
 }
