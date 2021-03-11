c272a626d75d76d0560499b04120561b0275f0d8
grangus <31970023+grangus@users.noreply.github.com>
2021-03-10 18:46:35 -0600

    updated translation properties

--- server/src/misc/translations/es/index.ts
+++ server/src/misc/translations/es/index.ts
@@ -2,4 +2,7 @@ export const lang = {
   joi_errors: {},
   unknown: "Unknown error!",
   internal_error: "Internal error!",
+  user_exists: "A user with that email already exists!",
+  invalid_email_or_password:
+    "The email or password provided does not match our records!",
 };
