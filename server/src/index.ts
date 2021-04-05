fe38ad894364c9c67e04173acdc46c3f96fb02b0
grangus <31970023+grangus@users.noreply.github.com>
2021-04-05 00:07:41 -0500

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -91,6 +91,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Origin",
     "https://legal-torrent.vercel.app"
   );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 app.use(routes);
