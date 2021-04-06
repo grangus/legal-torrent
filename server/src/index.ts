5ca170302a9eac98a2a42599ad8e2e7f3fb21d89
grangus <31970023+grangus@users.noreply.github.com>
2021-04-06 12:45:09 -0500

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -68,6 +68,7 @@ staticApp.get("*", (req, res) => {
 app.use(
   session({
     cookie: {
+      domain: ".f99.wtf",
       secure: true, //this should be set to "true" in production - only have it turned off because i dont have tls enabled
     },
     secret: "TEMPORARY_SECRET",
